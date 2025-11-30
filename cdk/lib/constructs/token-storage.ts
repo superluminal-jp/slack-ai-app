2bb194663507fd0af9ec3ffc09357ca27d7cb5ed
Taiki Ogihara <taiki.ogihara@gmail.com>
2025-11-30 21:08:08 +0900

    Update token-storage.ts: formatting and removal policy
    - Change RemovalPolicy from RETAIN to DESTROY for MVP
    - Apply consistent double-quote formatting

--- cdk/lib/constructs/token-storage.ts
+++ cdk/lib/constructs/token-storage.ts
@@ -1,6 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 
 export class TokenStorage extends Construct {
   public readonly table: dynamodb.Table;
@@ -9,16 +9,15 @@ export class TokenStorage extends Construct {
     super(scope, id);
 
     // Create DynamoDB table for workspace installation tokens
-    this.table = new dynamodb.Table(this, 'WorkspaceTokensTable', {
-      tableName: 'slack-workspace-tokens',
+    this.table = new dynamodb.Table(this, "WorkspaceTokensTable", {
+      tableName: "slack-workspace-tokens",
       partitionKey: {
-        name: 'team_id',
+        name: "team_id",
         type: dynamodb.AttributeType.STRING,
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       encryption: dynamodb.TableEncryption.AWS_MANAGED,
-      removalPolicy: cdk.RemovalPolicy.RETAIN, // Retain table on stack deletion
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Destroy table on stack deletion
     });
   }
 }
-
